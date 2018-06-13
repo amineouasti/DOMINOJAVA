0a29ebfd81a5ccb5c0dd66b83ab1567339db2038
amineouasti <aminemomo123@gmail.com>
2018-06-13 23:04:16 +0200

    Add files via upload

---
+++ JavaProjet/src/javaprojet/JavaProjet.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaprojet;
+
+/**
+ *
+ * @author marou
+ */
+public class JavaProjet {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        new mainmenu().setVisible(true);
+        // TODO code application logic here
+    }
+    
+}
